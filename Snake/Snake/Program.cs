0706fe552411f97ece6bc8b5e567f47ab48cb11a
SofiaZoom <sofiazoom.x99@gmail.com>
2017-08-04 19:29:18 +0300

    Звёзды не мертвы!

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Звёзды не мертвы!");
+            Console.ReadLine();
+        }
+    }
+}
